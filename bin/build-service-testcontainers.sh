6f914796878a0c322e237fcb5e9c86b8e50ad56f
Cora Iberkleid <ciberkleid@gmail.com>
2021-08-19 13:26:22 -0400

    WIP: docker in docker for kpack/TBS

---
+++ bin/build-service-testcontainers.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+docker build k8s/ --file k8s/Dockerfile --tag docker
+docker tag docker gcr.io/fe-ciberkleid/springone/docker
+docker push gcr.io/fe-ciberkleid/springone/docker
+
+kubectl create ns springone-message-board
+kubectl config set-context --current --namespace=springone-message-board
+
+#kubectl create deployment docker --image=ciberkleid/docker \
+#        --dry-run=client --output yaml > k8s/docker-deployment.yaml
+
+kubectl expose -f k8s/docker-deployment.yaml --name docker \
+        --protocol=TCP --port=80 --target-port=80 \
+        --type=LoadBalancer \
+        --dry-run=client --output yaml > k8s/docker-service.yaml
+
+kp image create springone-message-board-service \
+  --tag gcr.io/fe-ciberkleid/springone/springone-message-board-service \
+  --cluster-builder base \
+  --namespace springone-message-board \
+  --env BP_MAVEN_BUILD_ARGUMENTS='package' \
+  --env DOCKER_HOST=tcp://docker:80 \
+  --wait \
+  --git https://github.com/springone-2021-testcontainers/springone-message-board-service \
+  --git-revision main \
+  --dry-run --output  yaml > k8s/image.yaml
+
+kubectl apply -f k8s/docker-deployment.yaml
+kubectl apply -f k8s/docker-service.yaml
+
+kp secret create regcred --gcr /Users/ciberkleid/Downloads/fe-ciberkleid-c2db4d4e8708.json
+kubectl apply -f k8s/image.yaml
+
+kp build logs springone-message-board-service -b 1
+
+#kubectl delete -f k8s/
+#kubectl delete secret regcred
+#kubectl delete ns springone-message-board
+#kp image delete springone-message-board-service
--- src/main/java/springone/messageboardservice/MessageController.java
+++ src/main/java/springone/messageboardservice/MessageController.java
@@ -1,6 +1,5 @@
 package springone.messageboardservice;
 
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +15,14 @@ class MessageController {
     }
 
     @GetMapping(value = "", produces = "application/json;charset=UTF-8")
+    @ResponseBody
     List<Message> getMessages() {
         return this.service.getMessages();
     }
 
     @PostMapping(value = "", consumes = "application/json;charset=UTF-8",
         produces="application/json;charset=UTF-8")
+    @ResponseBody
     ApiResponse postMessage (@RequestBody CreateMessage newMessage) {
         ApiResponse response= new ApiResponse("0","Failure: Name must be Uppercase","Create");
         try {
@@ -33,8 +34,8 @@ class MessageController {
         return response;
     }
 
-    @DeleteMapping(value = "/{username}", consumes = "application/json;charset=UTF-8",
-            produces="application/json;charset=UTF-8")
+    @DeleteMapping(value = "/{username}", produces="application/json;charset=UTF-8")
+    @ResponseBody
     AdminApiResponse deleteMessageByUsername(@PathVariable String username) {
         AdminApiResponse response;
         try {
